c3c01c7bf37c4242dc423ed360f3e60863f6ceb3
Maxime Marcelin <maisonmarc7@gmail.com>
2026-08-27 16:38:25 +0200

    Done Initializing project.

--- backend/main.sh
+++ backend/main.sh
@@ -4,4 +4,4 @@ echo "Installing packages..."
 pip install -r ./requirements.txt
 
 echo "Launching API..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload --proxy-headers --ssl-certfile ./ssl/fullchain.pem --ssl-keyfile ./ssl/key.pem --log-level debug
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload --proxy-headers --log-level debug
